b03f85f64f1d5f6f2f2e676b04dcfd56d0c6a307
Your Name <your.email@example.com>
2016-06-28 18:12:51 +0800

    finssh

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,7 +7,7 @@ class SessionsController < ApplicationController
           if user.activated?
               log_in user
               params[:session][:remember_me] == '1' ? remember(user): forget(user)
-              redirect_back_or user
+              redirect_to user
           else
             message = " Account not activated.  "
             message += "check you email for the activation"
--- db/seeds.rb
+++ db/seeds.rb
@@ -10,19 +10,19 @@ User.create!(name:  "Example User",
              email: "example@railstutorial.org",
              password:              "foobar",
              password_confirmation: "foobar",
-             admin:     true,
-             activated: true,
-             activated_at: Time.zone.now)
+             admin:                 true,
+             activated:            true,
+             activated_at:       Time.zone.now)
 
 99.times do |n|
   name  = Faker::Name.name
   email = "example-#{n+1}@railstutorial.org"
   password = "password"
   User.create!(name:  name,
-              email: email,
+              email:                      email,
               password:              password,
               password_confirmation: password,
-              activated: true,
-              admin:   false,
+              activated:                false,
+              admin:                     false,
               activated_at: Time.zone.now)
 end
