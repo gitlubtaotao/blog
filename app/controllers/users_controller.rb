c023dfd03200fcb0941959857e71f7e327100cd2
Your Name <your.email@example.com>
2016-06-28 18:28:26 +0800

    Finish account-activation-password-redsds

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -22,8 +22,19 @@ class UsersController < ApplicationController
   		render 'new'
   	end 
   end
-  
-
+  def edit
+    @user = User.find(params[:id])
+  end  
+   def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(user_params)
+      # 处理更新成功的情况
+       flash[:success] = "Profile updated"
+       redirect_to @user
+    else
+      render 'edit'
+    end
+  end
   private
       def user_params
           params.require( :user).permit(:name, :email, :password, :password_confirmation)
