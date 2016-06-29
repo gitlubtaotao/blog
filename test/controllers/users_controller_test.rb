1ff0d028747b1ad4fd1273bcdbda6e09ecd20534
Your Name <your.email@example.com>
2016-06-29 14:43:36 +0800

    Add Micropost follow

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,9 +1,17 @@
 require 'test_helper'
 
 class UsersControllerTest < ActionController::TestCase
-  test "should get new" do
-    get :new
-    assert_response :success
-  end
+  	test "should get new" do
+   		get :new
+    		assert_response :success
+  	end
+	test "should redirect following when not logged in" do
+   		 get :following, id: @user
+    		assert_redirected_to login_url
+  	end
 
+  	test "should redirect followers when not logged in" do
+    		get :followers, id: @user
+    		assert_redirected_to login_url
+ 	 end
 end
